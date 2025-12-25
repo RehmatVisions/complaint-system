520580738ebddf6c1ea8c1e2c055d56b99e5bcc3
Rehmat Ali <itsrehmet@gmail.com>
2025-12-25 20:57:30 +0500

    db connected

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+// config/mongodb.js
+import mongoose from 'mongoose';
+
+// MongoDB connection URI
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mydatabase';
+
+// Function to connect to MongoDB
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
