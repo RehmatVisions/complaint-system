3a70c2c6c340345806f9fd974a34410f7ad2af79
ahmads01 <mahmadsaeed999@gmail.com>
2026-01-07 12:07:55 +0500

    Complete Complaint Module backend with CRUD, validation, and ownership middleware

--- Routes/ComplaintRoutes.js
+++ Routes/ComplaintRoutes.js
@@ -1,16 +1,28 @@
 import express from "express";
 import { authMiddleware } from "../Middlewares/AuthMiddleware.js";
 import { complaintOwnershipMiddleware } from "../Middlewares/OwnershipMiddleware.js";
+
 import {
   submitComplaint,
   getOwnComplaints,
-  getSingleComplaint
+  getSingleComplaint,
+  updateComplaint,
+  deleteComplaint
 } from "../Controllers/ComplaintController.js";
 
 const router = express.Router();
 
-router.post("/submit-complaint", submitComplaint);
-router.get("/get-owncomplaints", authMiddleware, getOwnComplaints);
-router.get("/get-single-complaint/:id", authMiddleware, complaintOwnershipMiddleware, getSingleComplaint);
+/* CREATE */
+router.post("/complaints", authMiddleware, submitComplaint);
+
+/* READ */
+router.get("/complaints", authMiddleware, getOwnComplaints);
+router.get( "/complaints/:id", authMiddleware, complaintOwnershipMiddleware, getSingleComplaint);
+
+/* UPDATE */
+router.put( "/complaints/:id", authMiddleware, complaintOwnershipMiddleware, updateComplaint);
+
+/* DELETE */
+router.delete( "/complaints/:id",authMiddleware, complaintOwnershipMiddleware, deleteComplaint);
 
 export default router;
